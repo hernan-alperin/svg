41fc802099c4f01a5abf6ee41e85dea49e0ca3ba
Hernán Alperin <hernan.alperin@gmail.com>
2014-11-27 10:19:15 -0500

    adjusted the colors

--- pyramid.js
+++ pyramid.js
@@ -8,7 +8,14 @@ function coor2point(x,y) {return ' ' + x.toString() + ',' + y.toString(); }
 console.log('<!DOCTYPE html>');
 console.log('<html>');
 console.log('<body>');
-console.log('<script>function paintDown(elem) {elem.style.fill="yellow";}</script>');
+
+var paintDown = 'function paintDown(i) {'
+  + ''
+  + ''
+  + ''
+  + '};';
+
+console.log('<script>function paintDown(element) {element.style.fill="forestgreen";}</script>');
 console.log('<svg '
   + 'width="' + width.toString() + '" '
   + 'height="' + width.toString() + '">');
@@ -16,12 +23,13 @@ for (var i=0; i<levels; i++) {
   var Dw = width/2; 
   var x0 = i/levels*Dw, x1 = (i+1)/levels*Dw, x2 = width-x1, x3 = width - x0;
   var y0 = y3 = (levels-i)/levels*height, y1 = y2 = (levels-i-1)/levels*height;
-  console.log('  <polygon id="level_' +i+ '" points="'
+  var level_id = 'level_'+i;
+  console.log('  <polygon id="'+level_id+' level="'+i+'" " points="'
     + coor2point(x0,y0)
     + coor2point(x1,y1)
     + coor2point(x2,y2)
     + coor2point(x3,y3)
-    + '" style="fill:lime;stroke:purple;stroke-width:1" '
+    + '" style="fill:lightgrey;stroke:white;stroke-width:1" '
     + 'onClick="paintDown(this)" '
     + '/>');
 }
