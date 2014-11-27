04b07444eaa33683f1a6b9a014541c9901e5068c
Hernán Alperin <hernan.alperin@gmail.com>
2014-11-27 12:06:48 -0500

    colorwise painting the stages this and bellow

--- pyramid.js
+++ pyramid.js
@@ -9,13 +9,18 @@ console.log('<!DOCTYPE html>');
 console.log('<html>');
 console.log('<body>');
 
-var paintDown = 'function paintDown(i) {'
-  + ''
-  + ''
-  + ''
-  + '};';
+var paintDown = '<script>'
+  + 'function paintDown(element) {\n'
+  + '  var stages = document.getElementsByClassName("pyramid-level");\n'
+  + '  var levels = stages.length;\n'
+  + '  var level = element.getAttribute("level");\n'
+  + '  for (i=0; i<=level; i++) stages[i].style.fill="forestgreen";\n'
+  + '  for (; i<levels; i++) stages[i].style.fill="lightgrey";\n'
+  + '};\n'
+  + '</script>';
 
-console.log('<script>function paintDown(element) {element.style.fill="forestgreen";}</script>');
+console.log(paintDown);
+//console.log('<script>function paintDown(element) {element.style.fill="forestgreen";}</script>');
 console.log('<svg '
   + 'width="' + width.toString() + '" '
   + 'height="' + width.toString() + '">');
@@ -24,12 +29,10 @@ for (var i=0; i<levels; i++) {
   var x0 = i/levels*Dw, x1 = (i+1)/levels*Dw, x2 = width-x1, x3 = width - x0;
   var y0 = y3 = (levels-i)/levels*height, y1 = y2 = (levels-i-1)/levels*height;
   var level_id = 'level_'+i;
-  console.log('  <polygon id="'+level_id+' level="'+i+'" " points="'
-    + coor2point(x0,y0)
-    + coor2point(x1,y1)
-    + coor2point(x2,y2)
-    + coor2point(x3,y3)
-    + '" style="fill:lightgrey;stroke:white;stroke-width:1" '
+  console.log('  <polygon id="' + level_id + '" class="pyramid-level" level="' + i + '" '
+    + 'points="' + coor2point(x0,y0) + coor2point(x1,y1) + coor2point(x2,y2) + coor2point(x3,y3) + '" ' 
+    + 'state="to-go" '
+    + 'style="fill:lightgrey;stroke:white;stroke-width:1" '
     + 'onClick="paintDown(this)" '
     + '/>');
 }
